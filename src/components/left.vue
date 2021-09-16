ca23c59d28d5240c741a3ad659025f9b43c9f34a
dudongzhen <dudongzhen@cmss.chinamobile.com>
2021-09-16 10:03:04 +0800

    add element plus

--- src/components/left.vue
+++ src/components/left.vue
@@ -1,5 +1,25 @@
 <template>
-    <div class="left-side">module-l</div>
+    <div class="left-side">
+      <el-menu
+        default-active="1-1-1"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <i class="el-icon-location"></i>
+            <span>Button 按钮</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1-1">基础用法</el-menu-item>
+            <el-menu-item index="1-2-1">禁用状态</el-menu-item>
+            <el-menu-item index="1-3-1">图标按钮</el-menu-item>
+            <el-menu-item index="1-4-1">按钮组</el-menu-item>
+          </el-menu-item-group>
+        </el-sub-menu>
+        <el-menu-item index="2">Icon 图标</el-menu-item>
+        <el-menu-item index="3">Input 输入框</el-menu-item>
+        <el-menu-item index="4">Switch 开关</el-menu-item>
+      </el-menu>
+    </div>
 </template>
 <script>
 export default {
@@ -16,6 +36,5 @@ export default {
   top: 0;
   width: 20%;
   height: 100%;
-  background: rgb(256, 0, 0);
 }
 </style>
